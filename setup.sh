a8538497f0b306c350fd6b65a0748f2c29eb99b1
Mohamed-Montasser <mmontasser43@gmail.com>
2025-05-09 08:12:57 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-# Install system dependencies
-sudo apt-get update
-sudo apt-get install -y libgl1-mesa-glx libglib2.0-0
-
-# Install Python packages
-pip install --upgrade pip
+# For Streamlit Sharing (no sudo available)
+apt-get update && apt-get install -y --no-install-recommends libgl1
 pip install -r requirements.txt
